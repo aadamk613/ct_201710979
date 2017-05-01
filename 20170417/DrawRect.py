2386adc4e7d3bdc0a5183e665147dcb82c3566dc
aadamk613 <aadamk@naver.com>
2017-05-01 16:43:12 +0900

    Add files via upload

---
+++ 20170417/DrawRect.py
@@ -0,0 +1,19 @@
+Python 2.7.6 (default, Nov 10 2013, 19:24:24) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> wn=turtle.Screen()
+>>> t1=turtle.Turtle()
+>>> t1.forward(100)
+>>> t1.right(90)
+>>> t1.forward(100)
+>>> t1.right(90)
+>>> t1.forward(100)
+>>> t1.right(90)
+>>> forward(100)
+
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    forward(100)
+NameError: name 'forward' is not defined
+>>> t1.forward(100)
+>>> 
